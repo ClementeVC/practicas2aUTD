17023f653f1b4ba748f5ce7c77cd0d47b4c715d2
Len <Len@DESKTOP-QTTCKLB>
2019-01-31 20:22:41 -0600

    as

--- src/programaSegundoA/Programa12a.java
+++ src/programaSegundoA/Programa12a.java
@@ -5,11 +5,33 @@ public class Programa12a {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner l = new Scanner(System.in);
-		String i="S";
-		
+		int a=0,b=0,c=0;
+		char i='S';
+		double x=0;
 		do{
+			System.out.println("venta:");
+			x=l.nextDouble();
 			
-		}while(i=="N");
+			if(x<=200){
+				System.out.println("La venta es menoy o igual a $200");
+				a++;
+			}
+			else if(x>200 && x<400){
+				System.out.println("La venta es mayor a $200 pero menor que $400");
+				b++;
+			}
+			else{
+				System.out.println("La venta fue mayor de $400");
+				c++;
+			}
+			
+			
+			System.out.println("hay otra venta?(S/N)");
+			i=l.next().charAt(0);
+		}while(i!='N');
+		System.out.println(a+" fueron menores o iguales a $200");
+		System.out.println(b+" fueron mayores a $200 y menores a $400");
+		System.out.println(c+" fueron mayores a $400");
 		
 	}
 
--- src/programaSegundoA/Programa12b.java
+++ src/programaSegundoA/Programa12b.java
@@ -4,7 +4,46 @@ public class Programa12b {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner le = new Scanner(System.in);
+		System.out.println("Cuantos clienter seran?");
+		int n = le.nextInt();
+		int m = 1;
+		int a,b,c=0;
+		//b acumulador total
+		//c promedio
+		int mayor=0,menor=0;
+		int t=0,pt=0,tp=0;
+		do{
+			byte x = 1;
+			b=0;
+			do{
+			
+			System.out.println("Consumo del cliente "+m+"en el mes "+x);
+			a=le.nextInt();
+			b+=a;
+			x++;
+			}while(x<=12);
+			c=b/12;
+			if(c>100){
+				mayor++;
+			}
+			else{
+				menor++;
+			}
+			System.out.println("total del cliente: "+b);
+			System.out.println("Promedio meses: "+c);
+			t+=b;
+			tp+=c;
+			
+			m++;
+		}while(m<=n);
+		tp/=n;
+		pt=t/n;
+		System.out.println("Total de minutos: "+t);
+		System.out.println("Promedio de los clientes: "+pt);
+		System.out.println("Promedio por mes: "+tp);
+		System.out.println("mas de 100 minutos al mes: "+mayor);
+		System.out.println("menos de 100 minutos al mes: "+menor);
 	}
 
 }
